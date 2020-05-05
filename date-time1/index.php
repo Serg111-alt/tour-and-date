88bd56fc6e72398a817577abd5dabd77320986f2
Serg111-alt <56454209+Serg111-alt@users.noreply.github.com>
2020-05-05 17:41:36 +0300

    Add files via upload

---
+++ date-time1/index.php
@@ -0,0 +1,12 @@
+<?php
+header("Refresh:1");
+$now = time();  
+$future = strtotime ("07-05-2020 15:00");
+$dif = $future - $now;
+$days = floor($dif/(24*60*60));
+$hours = floor(($dif/(3600)));
+$minutes = floor(($dif/(60)));
+$milisec = floor(($dif*(1000)));
+echo "До 07-05-2020 15:00 осталось:";
+echo "</br>";
+echo "Дни"." ".$days." "."Часы"." ".$hours." Минуты"." ".$minutes." "."Секунд"." ".$dif." "."Милисекунд"." ".$milisec;
